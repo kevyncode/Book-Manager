ba04f00a7d6ffbc387cae11a3a2a6f6e1a270dfe
Kevyncode <kevinsilvar82@gmail.com>
2024-08-22 13:12:06 -0300

    Members added

--- src/LibraryManagement/UtilMembers/Members.java
+++ src/LibraryManagement/UtilMembers/Members.java
@@ -1,4 +1,58 @@
 package LibraryManagement.UtilMembers;
 
 public class Members {
+
+    private String nameMember;
+    private int cpfMember;
+    private String emailMember;
+    private String phoneMember;
+    private String addressMember;
+
+    public Members(String nameMember, int cpfMember, String phoneMember, String emailMember, String addressMember) {
+        this.nameMember = nameMember;
+        this.cpfMember = cpfMember;
+        this.phoneMember = phoneMember;
+        this.emailMember = emailMember;
+        this.addressMember = addressMember;
+    }
+
+    public String getNameMember() {
+        return nameMember;
+    }
+
+    public void setNameMember(String nameMember) {
+        this.nameMember = nameMember;
+    }
+
+    public int getCpfMember() {
+        return cpfMember;
+    }
+
+    public void setCpfMember(int cpfMember) {
+        this.cpfMember = cpfMember;
+    }
+
+    public String getEmailMember() {
+        return emailMember;
+    }
+
+    public void setEmailMember(String emailMember) {
+        this.emailMember = emailMember;
+    }
+
+    public String getPhoneMember() {
+        return phoneMember;
+    }
+
+    public void setPhoneMember(String phoneMember) {
+        this.phoneMember = phoneMember;
+    }
+
+    public String getAddressMember() {
+        return addressMember;
+    }
+
+    public void setAddressMember(String addressMember) {
+        this.addressMember = addressMember;
+    }
 }
--- src/LibraryManagement/UtilMembers/MembersManagement.java
+++ src/LibraryManagement/UtilMembers/MembersManagement.java
@@ -1,4 +1,64 @@
 package LibraryManagement.UtilMembers;
 
-public class MembersManagement {
+import java.util.ArrayList;
+
+public class MembersManagement implements NewMembers{
+
+    private final ArrayList<Members> members;
+
+    public MembersManagement() {
+        members = new ArrayList<>();
+    }
+
+    @Override
+    public void registerNewMember(String nameMember, int cpfMember, String phoneMember, String emailMember, String addressMember) {
+        Members member = new Members(nameMember, cpfMember, phoneMember, emailMember, addressMember);
+        members.add(member);
+        System.out.println("Added member: " + member.getNameMember());
+    }
+
+    @Override
+    public void updateInformationMember(String nameMember, int cpfMember, String phoneMember, String emailMember, String addressMember) {
+        for (Members member : members) {
+            if (member.getCpfMember() == cpfMember) {
+                member.setNameMember(nameMember);
+                member.setPhoneMember(phoneMember);
+                member.setEmailMember(emailMember);
+                member.setAddressMember(addressMember);
+                System.out.println("Updated member: " + member.getNameMember());
+                return;
+            }
+        }
+        System.out.println("Member with CPF " + cpfMember + " not found.");
+    }
+
+    @Override
+    public void removeMember(int cpfMember) {
+        for (Members member : members) {
+            if (member.getCpfMember() == cpfMember) {
+                members.remove(member);
+                System.out.println("Removed member: " + member.getNameMember());
+                return;
+            }
+        }
+        System.out.println("Member with CPF " + cpfMember + " not found.");
+    }
+
+    @Override
+    public void printMembers() {
+        if (members.isEmpty()) {
+            System.out.println("No members available.");
+        } else {
+            for (Members member : members) {
+                System.out.println("=====================================");
+                System.out.println("Name: " + member.getNameMember());
+                System.out.println("CPF: " + member.getCpfMember());
+                System.out.println("Phone: " + member.getPhoneMember());
+                System.out.println("Email: " + member.getEmailMember());
+                System.out.println("Address: " + member.getAddressMember());
+                System.out.println("=====================================");
+            }
+        }
+    }
+
 }
